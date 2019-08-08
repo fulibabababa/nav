2fea1048a7bb0788f169658dba500c11be5e3268
zhangwei <zhangwei@qq.com>
2019-08-08 15:28:16 +0800

    haha

---
+++ database/migrations/2019_08_08_071112_create_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('web_name')->nullable();
+            $table->string('link')->nullable();
+            $table->bigInteger('category_id')->nullable();
+            $table->tinyInteger('status')->default(0)->nullable();
+            $table->unsignedBigInteger('view')->default(0)->nullable();
+            $table->string('ip')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
